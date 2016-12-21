5de518d23cebf80ccfb24e5a88fdb89a86e1a206
yoann <yoann.dugard@gmail.com>
2016-12-21 16:34:13 +0100

    debut affichage menu

--- w/app/Controller/DefaultController.php
+++ w/app/Controller/DefaultController.php
@@ -2,7 +2,11 @@
 
 namespace Controller;
 
+
+use Model\ProductsModel;
+use Model\CategoriesModel;
 use \W\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
@@ -14,5 +18,26 @@ class DefaultController extends Controller
 	{
 		$this->show('default/home');
 	}
+
+	public function displayMenu($idCategory = 1)
+	{
+		// Récupère les différents categories
+		// Il nous faut le modèle pour cela :
+		$categoryModel = new CategoriesModel();
+
+		$category = $categoryModel->findAll();
+
+	
+		// Récupère les différents menus
+		// Il nous faut le modèle pour cela :
+		$menuModel = new ProductsModel();
+		$menu = $menuModel->findProductsByCategory($idCategory);
+
+		$this->show('menu', ['allCategory' => $category, 'allMenu' => $menu] );
+
+
+	}
+
+
 
 }
