dda0a2a73b41cca8ef83c1d76394cc3f6d90e918
Mickael Lukasiewicz <mickael_lukasiewicz@hotmail.fr>
2016-12-22 10:12:53 +0100

    Layout base modifié

--- w/app/Views/admin/menu.php
+++ w/app/Views/admin/menu.php
@@ -8,7 +8,7 @@
   					<div class="col-xs-12 col-md-10 col-md-offset-2">
 	  					<div class="row" id="content_prod">
 	 						<?php foreach($allProducts as $product) : ?>
-	 							<div class="col-xs-4 col-md-3">
+	 							<div class="col-xs-12 col-md-3">
 	 								<img src="<?= $this->assetUrl('img/'. $product['productPicture']) ?>" width="200px" height="150px">
 	 							</div>
 	 						<?php endforeach ; ?>
