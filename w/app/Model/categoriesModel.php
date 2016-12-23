76a882e0e8ba90889f2540f3187d8ef7794d3b4b
Mickael Lukasiewicz <mickael_lukasiewicz@hotmail.fr>
2016-12-23 14:20:36 +0100

    Derniers changements

--- w/app/Model/categoriesModel.php
+++ w/app/Model/categoriesModel.php
@@ -2,10 +2,9 @@
 
 namespace Model;
 
-use \W\Model\Model;
+use W\Model\Model;
 
 class CategoriesModel extends Model
 {
-	// On va maintenant hériter de toutes les méthodes utiles de \W\Model\Model
-	// Pas besoin d'écrire de SQL pour le moment
+	
 }
